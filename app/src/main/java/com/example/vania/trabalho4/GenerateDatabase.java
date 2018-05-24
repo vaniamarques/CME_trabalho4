01f71f0f46fa1b61d18aa277b55556098e7612e9
vania <vaniamarques343@gmail.com>
2018-05-24 23:56:18 +0100

    Database for login

--- app/src/main/java/com/example/vania/trabalho4/GenerateDatabase.java
+++ app/src/main/java/com/example/vania/trabalho4/GenerateDatabase.java
@@ -1,6 +1,8 @@
 package com.example.vania.trabalho4;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -11,14 +13,86 @@ public class GenerateDatabase extends SQLiteOpenHelper {
     public GenerateDatabase(Context context) {
         super(context, DATABASE_NAME, null, VERSION);
     }
+
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String s ="CREATE TABLE despesas(_idDespesas integer primary key autoincrement, tipoDespesa varchar(40), valorDespesa double, dataDespesa varchar(10), horaDespesa varchar(5), anexoDespesa varchar(255))";
-        db.execSQL(s);
+        String create_table_expenses = "CREATE TABLE despesas(_idDespesas integer primary key autoincrement, tipoDespesa varchar(40), valorDespesa double, dataDespesa varchar(10), horaDespesa varchar(5), anexoDespesa varchar(255))";
+
+        String create_table_users = "CREATE TABLE users (id integer primary key autoincrement, username varchar(40), email varchar(255), password varchar(255))";
+
+
+        String[] create_table = new String[]{create_table_expenses, create_table_users};
+
+        for(String sql : create_table){
+            db.execSQL(sql);
+        }
     }
+
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS despesas");
+
+        String drop_table_expenses = "DROP TABLE IF EXISTS despesas";
+
+        String drop_table_users = "DROP TABLE IF EXISTS users";
+
+        String[] drop_table = new String[]{drop_table_expenses, drop_table_users};
+
+        for(String sql : drop_table){
+            db.execSQL(sql);
+        }
         onCreate(db);
     }
+
+
+
+
+    public void addUser(User user) {
+
+        SQLiteDatabase db = this.getWritableDatabase();
+
+
+        ContentValues values = new ContentValues();
+
+        values.put("username", user.userName);
+        values.put("email", user.email);
+        values.put("password", user.password);
+
+        long todo_id = db.insert("users", null, values);
+    }
+
+    public User Authenticate(User user) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.query("users",
+                new String[]{"id", "name", "email", "password"},
+                "email" + "=?",
+                new String[]{user.email},//Where clause
+                null, null, null);
+
+        if (cursor != null && cursor.moveToFirst()&& cursor.getCount()>0) {
+            User user1 = new User(cursor.getString(0), cursor.getString(1), cursor.getString(2), cursor.getString(3));
+
+            if (user.password.equalsIgnoreCase(user1.password)) {
+                return user1;
+            }
+        }
+        return null;
+    }
+
+    public boolean isEmailExists(String email) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.query("users",// Selecting Table
+                new String[]{"id", "name", "email", "password"},
+                "email" + "=?",
+                new String[]{email},//Where clause
+                null, null, null);
+
+        if (cursor != null && cursor.moveToFirst()&& cursor.getCount()>0) {
+
+            return true;
+        }
+
+        return false;
+    }
+
 }
+
