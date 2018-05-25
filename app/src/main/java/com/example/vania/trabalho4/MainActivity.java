fcc2fe9785905713e66d1f9e7fa391c4d8a4654a
vania <vaniamarques343@gmail.com>
2018-05-25 14:27:55 +0100

    Login update

--- app/src/main/java/com/example/vania/trabalho4/MainActivity.java
+++ app/src/main/java/com/example/vania/trabalho4/MainActivity.java
@@ -1,13 +1,106 @@
 package com.example.vania.trabalho4;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.Spanned;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.regex.Pattern;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText editTextEmail;
+    EditText editTextPassword;
+    Button buttonLogin;
+    GenerateDatabase sqliteHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sqliteHelper = new GenerateDatabase(this);
+
+        initViews();
+
+        //set click event of login button
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+
+            public void onClick(View view) {
+
+
+                String Email = editTextEmail.getText().toString();
+                String Password = editTextPassword.getText().toString();
+
+                if(isValidEmail(Email)) {
+
+                    Context context = getApplicationContext();
+                    CharSequence text = "Entra aqui!";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                }
+                else{
+                    Context context = getApplicationContext();
+                    CharSequence text = "Por favor, insira um email valido!";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                }
+
+               /* if (validate()) {
+
+
+                    User currentUser = sqliteHelper.Authenticate(new User(null, null, Email, Password));
+
+                    if (currentUser != null) {
+                        Context context = getApplicationContext();
+                        CharSequence text = "Succesfly login!";
+                        int duration = Toast.LENGTH_SHORT;
+
+                        Toast toast = Toast.makeText(context, text, duration);
+                        toast.show();
+
+                    } else {
+
+                        Context context = getApplicationContext();
+                        CharSequence text = "Failed Login!";
+                        int duration = Toast.LENGTH_SHORT;
+
+                        Toast toast = Toast.makeText(context, text, duration);
+                        toast.show();
+
+                    }
+                }*/
+
+            }
+        });
     }
+
+
+
+
+    //this method is used to connect XML views to its Objects
+    private void initViews() {
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        buttonLogin = (Button) findViewById(R.id.buttonLogin);
+
+    }
+    private boolean isValidEmail(String email) {
+        Pattern pattern = Patterns.EMAIL_ADDRESS;
+        return pattern.matcher(email).matches();
+    }
+
+
 }
