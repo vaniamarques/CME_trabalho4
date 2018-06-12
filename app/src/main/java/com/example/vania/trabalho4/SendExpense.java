dbe29e3ee2b9aadd4b6d80f4be7f143e1967d940
tiagofppereira <tiagofpp4@gmail.com>
2018-06-12 03:36:32 +0100

    With E-mail

--- app/src/main/java/com/example/vania/trabalho4/SendExpense.java
+++ app/src/main/java/com/example/vania/trabalho4/SendExpense.java
@@ -3,6 +3,8 @@ package com.example.vania.trabalho4;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Environment;
+import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -17,6 +19,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.io.File;
 
 public class SendExpense extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -65,6 +69,13 @@ public class SendExpense extends AppCompatActivity
                 sendEmail();
             }
         });
+
+        Attachment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFolder();
+            }
+        });
     }
 
     @Override
@@ -110,37 +121,66 @@ public class SendExpense extends AppCompatActivity
     protected void onActivityResult(int requestCode, int resultCode, Intent data)
     {
         if (requestCode == PICK_FROM_GALLERY && resultCode == RESULT_OK) {
+
             Uri selectedImage = data.getData();
+
+            // Will return "image:x*"
+            String wholeID = DocumentsContract.getDocumentId(selectedImage);
+            // Split at colon, use second item in the array
+            String id = wholeID.split(":")[1];
+            // where id is equal to
+            String sel = MediaStore.Images.Media._ID + "=?";
+
+
             String[] filePathColumn = { MediaStore.Images.Media.DATA };
-            Cursor cursor = getContentResolver().query(selectedImage,filePathColumn, null, null, null);
-            cursor.moveToFirst();
+            Cursor cursor = getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,filePathColumn, sel, new String [] { id }, null);
+
             columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-            attachmentFile = cursor.getString(columnIndex);
-            Log.e("Attachment Path:", attachmentFile);
-            URI = Uri.parse("file://" + attachmentFile);
+            if (cursor.moveToFirst()) {
+                URI = Uri.parse("file://" + cursor.getString(columnIndex));
+            }
+
             cursor.close();
         }
+
     }
     public void sendEmail()
     {
-        try
-        {
+        try {
             email = et_email.getText().toString();
             subject = et_subject.getText().toString();
             message = et_message.getText().toString();
-            final Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
+            final Intent emailIntent = new Intent(
+                    android.content.Intent.ACTION_SEND);
             emailIntent.setType("plain/text");
-            emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL,new String[] { email });
-            emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT,subject);
+            emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL,
+            new String[] { email });
+            emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT,
+                    subject);
             if (URI != null) {
                 emailIntent.putExtra(Intent.EXTRA_STREAM, URI);
             }
-            emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, message);
-            this.startActivity(Intent.createChooser(emailIntent,"Sending email..."));
+            emailIntent
+                    .putExtra(android.content.Intent.EXTRA_TEXT, message);
+            this.startActivity(Intent.createChooser(emailIntent,
+                    "Sending email..."));
+
+        } catch (Throwable t) {
+            Toast.makeText(this,
+                    "Request failed try again: " + t.toString(),
+                    Toast.LENGTH_LONG).show();
         }
-        catch (Throwable t)
-        {
-            Toast.makeText(this, "Request failed try again: " + t.toString(),Toast.LENGTH_LONG).show();
-        }
+
+    }
+
+    public void openFolder()
+    {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        intent.putExtra("return-data", true);
+        startActivityForResult(
+                Intent.createChooser(intent, "Complete action using"),
+                PICK_FROM_GALLERY);
     }
 }
